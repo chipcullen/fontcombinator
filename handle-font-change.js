69e3405adfe78836caf3e9b12b3229a33083fe14
Chip Cullen <ccullen@pbs.org>
2025-05-23 17:23:06 -0400

    bug

--- handle-font-change.js
+++ handle-font-change.js
@@ -29,5 +29,10 @@ document.addEventListener(FONT_CHANGE_EVENT, (e) => {
   if (existingLink) existingLink.remove();
   // add new link to google fonts with text set to null, and added axes
   buildLinkToGoogleFonts(selectedFont, null, axesString);
-  document.querySelector(target).style.fontFamily = selectedFontObject.family;
+  console.log(selectedFontObject.family);
+  document.querySelector(
+    target
+    // this string treatment is weird, but we need it in case the font family has a number
+    // in it's name. JS doesn't like that out of the box.
+  ).style.fontFamily = `'${selectedFontObject.family}'`;
 });
