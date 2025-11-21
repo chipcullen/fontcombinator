9bd22df19eed322a50fb36449de96d5867f04bb0
Chip Cullen <ccullen@pbs.org>
2025-11-21 16:47:39 -0500

    default config tweak

--- default-config.js
+++ default-config.js
@@ -13,7 +13,7 @@ const defaultConfig = {
     fontWeight: "700",
     lineHeight: "1.1",
     textBoxEdge: "text alphabetic",
-    textBoxTrim: "trim-both",
+    textBoxTrim: "none",
   },
   h2: {
     text: "You can edit this page",
@@ -26,7 +26,7 @@ const defaultConfig = {
     fontWeight: "700",
     lineHeight: "1.4",
     textBoxEdge: "text alphabetic",
-    textBoxTrim: "trim-both",
+    textBoxTrim: "none",
   },
   p: {
     text: "Donec ante. Sed at velit. Vestibulum at purus at urna porttitor sodales. Nullam pulvinar, urna interdum eleifend sodales, eros est tempus quam, quis ultricies nibh elit vitae urna. Donec pretium arcu at quam. Quisque tristique, lacus id tempor blandit, quam massa imperdiet lorem, porta fermentum quam ante ac tortor. Curabitur mauris lectus, dapibus ut, ornare sit amet, vulputate sit amet, erat.",
@@ -39,7 +39,7 @@ const defaultConfig = {
     fontWeight: "400",
     lineHeight: "1.4",
     textBoxEdge: "text alphabetic",
-    textBoxTrim: "trim-both",
+    textBoxTrim: "none",
   },
 };
 export { defaultConfig };
